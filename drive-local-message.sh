a5f0af932e919350e7fc8665c51b35656d8f1b13
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 19:15:01 +0800

    Delete drive-local-message.sh

--- drive-local-message.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Assumes you pass in an event type and a payload. Sends it to localhost.
-EVENT=$1
-PAYLOAD=$2
-
-cat $PAYLOAD | http -v POST http://localhost:5000/webhook X-GitHub-Event:$EVENT
