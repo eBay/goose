b435f50fcba192a183b13e3ec4e51e7740d707d4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 18:51:13 +0800

    Delete goose directory

--- goose/json_logs_test.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import MagicMock
-from .json_logs import JsonFormatter
-
-
-def test_formatter():
-    jf = JsonFormatter()
-    record = {}
-    record = MagicMock()
-    jf.add_fields(record, record, {})
